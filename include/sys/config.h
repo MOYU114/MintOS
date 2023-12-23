05cebdf61f1a7c5680da8eb81a7c49fb2846f2aa
Smoouns <2096674632@qq.com>
2023-12-23 04:42:37 -0800

    添加可执行程序rm

--- include/sys/config.h
+++ include/sys/config.h
@@ -37,7 +37,7 @@
 /*
  * disk log
  */
-#define ENABLE_DISK_LOG
+//#define ENABLE_DISK_LOG
 #define SET_LOG_SECT_SMAP_AT_STARTUP
 #define MEMSET_LOG_SECTS
 #define	NR_SECTS_FOR_LOG		NR_DEFAULT_FILE_SECTS
