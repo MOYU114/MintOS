28c8ce2c9d5c6849b191194d518e733fac316c8a
MOYU114 <moyu114@outlook.com>
2024-01-02 16:41:10 +0800

    fix stackoverflow

--- StackOverflow/command/stackoverflow.c
+++ StackOverflow/command/stackoverflow.c
@@ -5,9 +5,8 @@ void cover_addr(){//在本代码中其返回地址为0x7:0x01b1
     //BREAKPOINT;
     char buf[8]="12345678";
     char payload[] = {//在buf头与返回地址间，存在着ebp ebx 8个buf数据，共4*6=24位
-        0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41,
-        0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41,
-        0xb1, 0x10, 0x00};
+        'T','h','i','s',' ','i','s',' ','p','a','y','l','o','a','d','.',
+        ' ',' ',' ',' ',0xb1, 0x10, 0x00};
     BREAKPOINT;
     strcpy(buf,payload);
     BREAKPOINT;
@@ -19,9 +18,6 @@ void stackoverflow(){
     int num=0x12345678;
     BREAKPOINT;
     printf("overflow!\n");
-    int i = 10000;
-    while (i--)
-        ;
     for (;;);
     exit(0);
 }
